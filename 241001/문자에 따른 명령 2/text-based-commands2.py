8c484cbd4339d26e7b024a5d989b11c30aa0a366
Raphaely0929 <raphaely0929@naver.com>
2024-10-01 20:22:21 +0900

    Title: 문자에 따른 명령 2, Time: 116ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 241001/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,17 @@
+c = input()
+x, y = 0, 0
+dir = 0
+
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+for i in c:
+    if i == 'L':
+        dir = (dir - 1) % 4
+    elif i == 'R':
+        dir = (dir + 1) % 4
+    elif i == 'F':
+        x += dx[dir]
+        y += dy[dir]
+
+print(x, y)
