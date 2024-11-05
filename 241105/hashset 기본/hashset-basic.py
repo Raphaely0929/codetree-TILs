20178bac2a96497784d01d66212224acd076a75a
Raphaely0929 <raphaely0929@naver.com>
2024-11-05 20:50:29 +0900

    Title: hashset 기본, Time: 209ms, Memory: 23MB, Status: Passed - CodeTree

---
+++ 241105/hashset 기본/hashset-basic.py
@@ -0,0 +1,16 @@
+n = int(input())
+s = set()
+
+for _ in range(n):
+    command, x = tuple(input().split())
+    x = int(x)
+
+    if command == "add":
+        s.add(x)
+    elif command == "remove":
+        s.remove(x)
+    else:
+        if x in s:
+            print("true")
+        else:
+            print("false")
